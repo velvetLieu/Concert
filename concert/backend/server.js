96a32c709b47f4e4425c0f5c82f01c036f5a122b
Coffee-Please <priya.singh.310@my.csun.edu>
2021-02-28 16:01:00 -0800

    Connected front and backend.

--- concert/backend/server.js
+++ concert/backend/server.js
@@ -1,4 +1,4 @@
-// REQUIRED PACKAGES, FILES AND CONST VARS
+// REQUIRED SERVER PACKAGES, FILES AND CONST VARS
 const PATH = require('path'); // NodeJS - Handle and transforms filepaths
 const HTTP = require('http'); // NodeJS - Allow transfer of data over HTTP
 const EXPRESS = require('express'); // use express framework
@@ -7,25 +7,31 @@ const SERVER = HTTP.createServer(APP); // create the server to use for socket.io
 const PORT =  process.env.PORT ||3000; // Run server on available port, otherwise localhost fallback
 const ENV = process.env.NODE_ENV; // Set available port
 
-//use mongoose
-const mongoose = require('mongoose');
 
-const url = "mongodb+srv://root:CSUNconcert491@cluster0.bgl0c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
+// APP.use(cors());
+// Database consts
+//const mongoose = require('mongoose'); //use mongoose framework
+//const url = "mongodb+srv://root:CSUNconcert491@cluster0.bgl0c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" // Database Creds
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("We are connected!");
-});
+// DATABASE
+//mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}); // Connect to database
 
+//const db = mongoose.connection; // Store connection result
+
+//db.on('error', console.error.bind(console, 'connection error:')); // Catch connection error
+
+// Otherwose, confirm connection
+//db.once('open', function() {
+  //console.log("We are connected!");
+//});
+
+// SERVER
 // Create the http server running on the available port
 SERVER.listen(PORT);
 
 // Access heroku production environment or localhost
 if (ENV === 'production' || PORT === 3000) {
-    // Set code folder
-    APP.use(EXPRESS.static(PATH.join(__dirname, '../../public')));
+    // Set production code folder (concert/build)
+    APP.use(EXPRESS.static(PATH.join(__dirname, '../build')));
     console.log(`Server running on port:  ${PORT}`);
 } // end if
-
